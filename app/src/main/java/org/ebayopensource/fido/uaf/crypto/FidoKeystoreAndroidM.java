e5fe9ebd9850705bc1bd4b3ba50df7ae2100db3a
BenzeneSnake <kelly5142200@gmail.com>
2025-11-07 16:06:26 +0800

    [android升級]  fix: android -> androidx，Google 在 Android Studio 3.2 (2018) 推出新的架構，把所有 support library 遷移到新命名空間 androidx.*

--- app/src/main/java/org/ebayopensource/fido/uaf/crypto/FidoKeystoreAndroidM.java
+++ app/src/main/java/org/ebayopensource/fido/uaf/crypto/FidoKeystoreAndroidM.java
@@ -4,8 +4,8 @@ import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import android.util.Log;
 
 import java.io.IOException;
